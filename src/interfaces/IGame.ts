6a2405576836bffb7a1ed317d254465f394f564e
Martin Stickley <mds1966@gmail.com>
2022-08-06 11:10:17 +0100

    Updated description of the IGame interface and the composite hierarchy that will draw the UI

--- src/interfaces/IGame.ts
+++ src/interfaces/IGame.ts
@@ -4,9 +4,13 @@ import { CanvasManager } from '../managers/canvasmanager'
 //
 // Interface defining a game
 //
-// Game drawing will use a composite pattern where a ball
-// will be made up of a background color, a foreground value and possibly 
-// some decoration.
+// Game drawing will use a composite pattern where a game
+// will be made up of:
+//   Game
+//     Board
+//       Background color 
+//       Foreground value
+//       Decoration.
 //
 // Each of those classes will implement this draw method.
 //
